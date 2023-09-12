e5f41e40f5710e40a3d4adfcff508f108acbdb00
Assofac78 <assofacnanterre78@gmail.com>
2023-09-12 15:18:53 +0200

    deuxieme commit

---
+++ calcul.py
@@ -0,0 +1,7 @@
+nombre1 = float(input("Quel est la première valeur : "))
+
+nombre2 = float(input("Entrez la deuxième valeur : "))
+
+resultat = nombre1 * nombre2
+
+print("Le résultat de la multiplication est :", resultat)
